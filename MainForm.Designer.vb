31d7de1a8514d117d5219cdf77a33ec6c04eb301
UkokuGemini <ukoku@live.cn>
2023-12-16 00:56:49 +0800

    添加项目文件。

---
+++ MainForm.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.TextBox_Log = New System.Windows.Forms.TextBox()
+        Me.RichTextBox_Info = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.立即查询ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.立即标识ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripStatusLabel_Next = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel_Res = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.StatusStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel_Res, Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel_Next})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 329)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(842, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'TextBox_Log
+        '
+        Me.TextBox_Log.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox_Log.Dock = System.Windows.Forms.DockStyle.Right
+        Me.TextBox_Log.Location = New System.Drawing.Point(515, 24)
+        Me.TextBox_Log.Multiline = True
+        Me.TextBox_Log.Name = "TextBox_Log"
+        Me.TextBox_Log.Size = New System.Drawing.Size(327, 305)
+        Me.TextBox_Log.TabIndex = 3
+        '
+        'RichTextBox_Info
+        '
+        Me.RichTextBox_Info.BackColor = System.Drawing.SystemColors.Control
+        Me.RichTextBox_Info.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox_Info.Location = New System.Drawing.Point(0, 24)
+        Me.RichTextBox_Info.Name = "RichTextBox_Info"
+        Me.RichTextBox_Info.Size = New System.Drawing.Size(515, 305)
+        Me.RichTextBox_Info.TabIndex = 4
+        Me.RichTextBox_Info.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.立即查询ToolStripMenuItem, Me.立即标识ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(842, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '立即查询ToolStripMenuItem
+        '
+        Me.立即查询ToolStripMenuItem.Name = "立即查询ToolStripMenuItem"
+        Me.立即查询ToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.立即查询ToolStripMenuItem.Text = "立即查询"
+        '
+        '立即标识ToolStripMenuItem
+        '
+        Me.立即标识ToolStripMenuItem.Name = "立即标识ToolStripMenuItem"
+        Me.立即标识ToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.立即标识ToolStripMenuItem.Text = "立即标识"
+        '
+        'ToolStripStatusLabel_Next
+        '
+        Me.ToolStripStatusLabel_Next.Name = "ToolStripStatusLabel_Next"
+        Me.ToolStripStatusLabel_Next.Size = New System.Drawing.Size(34, 17)
+        Me.ToolStripStatusLabel_Next.Text = "Next"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(766, 17)
+        Me.ToolStripStatusLabel2.Spring = True
+        Me.ToolStripStatusLabel2.Text = "."
+        '
+        'ToolStripStatusLabel_Res
+        '
+        Me.ToolStripStatusLabel_Res.Name = "ToolStripStatusLabel_Res"
+        Me.ToolStripStatusLabel_Res.Size = New System.Drawing.Size(27, 17)
+        Me.ToolStripStatusLabel_Res.Text = "Res"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "InfoReporter"
+        Me.NotifyIcon1.Visible = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(842, 351)
+        Me.Controls.Add(Me.RichTextBox_Info)
+        Me.Controls.Add(Me.TextBox_Log)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "InfoReporter"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents TextBox_Log As TextBox
+    Friend WithEvents RichTextBox_Info As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 立即查询ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 立即标识ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripStatusLabel_Next As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel_Res As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,226 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Net
+Imports System.Net.NetworkInformation
+Imports System.Reflection.Emit
+Imports System.Text
+Imports System.Xml
+Imports System.Xml.Serialization
+Imports NativeWifi
+
+Public Class MainForm
+    Const HeadText As String = "<?xml version=" & Chr(34) & "1.0" & Chr(34) & "?>"
+    Const DesFileName As String = "Info.xml"
+    Dim InfoPath As String = Directory.GetCurrentDirectory & "\Info.xml"
+    Dim WebDavUrl, WebDavUser, WebDavPassword As String
+    Dim TickInterval As Integer = 1
+    Dim ContentAll As String = ""
+    Dim Once As Boolean = True
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "InfoRepoter" & "[Ver." & My.Application.Info.Version.ToString & "]"
+        ReadSettingXml()
+        InfoRecord()
+        Timer1.Interval = 3000
+        Timer1.Enabled = True
+        Me.CenterToScreen()
+    End Sub
+    Sub InfoRecord()
+        RichTextBox_Info.Text = ""
+        AppendAllText(HeadText & vbCrLf & "<InfoRepoter>" & vbCrLf & "<HostName>")
+        AppendAllText(Dns.GetHostName, True)
+        AppendAllText("</HostName>" & vbCrLf)
+        AppendAllText("<Wlan>")
+        AppendAllText(GetWifi(), True)
+        AppendAllText("</Wlan>" & vbCrLf & "<HostIp>" & vbCrLf)
+        For i = 0 To Dns.GetHostEntry(Dns.GetHostName).AddressList.Count - 1
+            AppendAllText("<Ip>")
+            AppendAllText(Dns.GetHostEntry(Dns.GetHostName).AddressList(i).ToString, True)
+            AppendAllText("</Ip>" & vbCrLf)
+        Next
+        AppendAllText("</HostIp>" & vbCrLf & "<Time>")
+        AppendAllText(Format(Now, "yyyy-MM-dd HH:mm:ss"), True)
+        AppendAllText("</Time>" & vbCrLf & "</InfoRepoter>")
+        IO.File.WriteAllText(InfoPath, ContentAll)
+    End Sub
+    Public Sub AppendAllText(ByVal Str As String, Optional Mark As Boolean = False)
+        If Mark Then
+            RichTextBox_Info.Select(RichTextBox_Info.Text.Length, 0) '选中开始
+            RichTextBox_Info.SelectionFont = New Font(RichTextBox_Info.Font, FontStyle.Bold)
+            RichTextBox_Info.SelectionColor = Color.DodgerBlue
+            RichTextBox_Info.AppendText(Str)
+            RichTextBox_Info.Select(RichTextBox_Info.Text.Length, 0)
+            RichTextBox_Info.SelectionFont = RichTextBox_Info.Font
+            RichTextBox_Info.SelectionColor = Color.Black
+        Else
+            RichTextBox_Info.AppendText(Str)
+        End If
+        ContentAll &= Str
+    End Sub
+    Function CompareInfo() As Boolean
+        Dim XmlArr As New ArrayList
+        If IO.File.Exists(InfoPath) Then
+            Try
+                Dim SettingXml As String = IO.File.ReadAllText(InfoPath)
+                If SettingXml.Length > 0 Then
+                    Dim xmlDoc As New XmlDocument()
+                    xmlDoc.Load(InfoPath)
+                    Dim IpStr As String = CType(xmlDoc.SelectSingleNode("InfoRepoter").SelectSingleNode("HostIp"), XmlElement).InnerXml
+                    Dim SplitIpStr As String() = IpStr.Replace("<Ip>", "").Split("</Ip>")
+                    For i = 0 To SplitIpStr.Count - 1
+                        XmlArr.Add(SplitIpStr(i).Replace("/Ip>", ""))
+                    Next
+                End If
+            Catch ex As Exception
+            End Try
+            Dim IpArr As New ArrayList
+            For i = 0 To Dns.GetHostEntry(Dns.GetHostName).AddressList.Count - 1
+                IpArr.Add(Dns.GetHostEntry(Dns.GetHostName).AddressList(i).ToString)
+            Next
+            Dim Res As Boolean = False
+            Dim FitNum As Integer = 0
+            For i = 0 To IpArr.Count - 1
+                If XmlArr.Contains(IpArr(i).ToString) Then
+                    FitNum += 1
+                End If
+            Next
+            If FitNum < IpArr.Count Then
+                Return False
+            Else
+                Return True
+            End If
+        Else
+            Return False
+        End If
+    End Function
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Interval = 60000 * Math.Max(TickInterval, 1)
+        If Once = False AndAlso CompareInfo() Then
+            ToolStripStatusLabel_Res.Text = "【" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "】:无变更."
+        Else
+            Once = False
+            UploadWebDAVFile(InfoPath, DesFileName, WebDavUrl & "/InfoRecord/" & Dns.GetHostName & "/", WebDavUser, WebDavPassword)
+            ToolStripStatusLabel_Res.Text = "【" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "】:已上传信息."
+        End If
+        ToolStripStatusLabel_Next.Text = "下次检查时间:【" & Format(DateAdd(DateInterval.Second, 0.001 * Timer1.Interval, Now), "yyyy-MM-dd HH:mm:ss") & "】"
+    End Sub
+    Private Sub 立即查询ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 立即查询ToolStripMenuItem.Click
+        InfoRecord()
+    End Sub
+    Private Sub 立即标识ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 立即标识ToolStripMenuItem.Click
+        Timer1_Tick(Nothing, Nothing)
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.WindowState = FormWindowState.Normal
+            Me.CenterToScreen()
+        ElseIf e.Button = Windows.Forms.MouseButtons.Right Then
+        End If
+    End Sub
+    Function GetWifi()
+        Dim SSIDName As String = ""
+        Dim wlan = New WlanClient()
+        Dim connectedSsids = New List(Of String)
+        SSIDName = ""
+        For Each wlanInterface As WlanClient.WlanInterface In wlan.Interfaces
+            Dim ssid As Wlan.Dot11Ssid = wlanInterface.CurrentConnection.wlanAssociationAttributes.dot11Ssid
+            connectedSsids.Add(New [String](Encoding.ASCII.GetChars(ssid.SSID, 0, CInt(ssid.SSIDLength))))
+            For Each item As String In connectedSsids
+                SSIDName += item
+            Next
+        Next
+        Return SSIDName
+    End Function
+    Private Sub MainForm_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            ShowInTaskbar = False
+        Else
+            ShowInTaskbar = True
+        End If
+    End Sub
+
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        NotifyIcon1.Visible = False
+        NotifyIcon1.Dispose()
+    End Sub
+
+    Private Sub TextBox_Log_TextChanged(sender As Object, e As EventArgs) Handles TextBox_Log.TextChanged
+        TextBox_Log.SelectionStart = TextBox_Log.Text.Length
+    End Sub
+
+    Function ReadSettingXml() As Boolean
+        Dim Res As Boolean = True
+        Dim SettingPath As String = Directory.GetCurrentDirectory & "\InfoReporter_Settings.Xml"
+        Try
+            Dim SettingXml As String = ""
+            If IO.File.Exists(SettingPath) Then
+                SettingXml = IO.File.ReadAllText(SettingPath)
+            End If
+            If SettingXml.Length > 0 Then
+                Dim xmlDoc As New XmlDocument()
+                xmlDoc.Load(SettingPath)
+                WebDavUrl = CType(xmlDoc.SelectSingleNode("InfoRepoter").SelectSingleNode("WebDavUrl"), XmlElement).InnerText
+                WebDavUser = CType(xmlDoc.SelectSingleNode("InfoRepoter").SelectSingleNode("WebDavUser"), XmlElement).InnerText
+                WebDavPassword = CType(xmlDoc.SelectSingleNode("InfoRepoter").SelectSingleNode("WebDavPassword"), XmlElement).InnerText
+                TickInterval = Convert.ToInt32(CType(xmlDoc.SelectSingleNode("InfoRepoter").SelectSingleNode("TickInterval"), XmlElement).InnerText)
+            End If
+        Catch ex As Exception
+            Res = False
+        End Try
+        Return Res
+    End Function
+    Public Function UploadWebDAVFile(ByVal fileToUpload As String, ByVal DesName As String, ByVal UpUrl As String, ByVal UserName As String, ByVal PassWord As String, Optional Log As Boolean = False) As Boolean
+        Dim Success As Boolean = False
+        Dim fileLength As Long = My.Computer.FileSystem.GetFileInfo(fileToUpload).Length
+        UpUrl = UpUrl.TrimEnd("/"c) & "/" & DesName
+        Dim request As HttpWebRequest = DirectCast(System.Net.HttpWebRequest.Create(UpUrl), HttpWebRequest)
+        request.Credentials = New NetworkCredential(UserName.Trim(), PassWord.Trim())
+        request.Method = WebRequestMethods.Http.Put
+        request.ContentLength = fileLength
+        '*** This is required for our WebDav server ***
+        request.SendChunked = True
+        request.Headers.Add("Translate: f")
+        request.AllowWriteStreamBuffering = True
+        Dim s As IO.Stream = Nothing
+        Try
+            s = request.GetRequestStream()
+        Catch ex As Exception
+            TextBox_Log.Text &= vbCrLf & ">>" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "【WevDav】:上传错误." & ex.Message
+        End Try
+        Dim fs As New IO.FileStream(fileToUpload, IO.FileMode.Open, IO.FileAccess.Read)
+        Dim byteTransferRate As Integer = 1024
+        Dim bytes(byteTransferRate - 1) As Byte
+        Dim bytesRead As Integer = 0
+        Dim totalBytesRead As Long = 0
+        Do
+            bytesRead = fs.Read(bytes, 0, bytes.Length)
+            If bytesRead > 0 Then
+                totalBytesRead += bytesRead
+                s.Write(bytes, 0, bytesRead)
+            End If
+        Loop While bytesRead > 0
+        s.Close()
+        s.Dispose()
+        s = Nothing
+        fs.Close()
+        fs.Dispose()
+        fs = Nothing
+        Dim response As HttpWebResponse = Nothing
+        Try
+            response = DirectCast(request.GetResponse(), HttpWebResponse)
+            Dim code As HttpStatusCode = response.StatusCode
+            response.Close()
+            response = Nothing
+            If totalBytesRead = fileLength AndAlso code = HttpStatusCode.Created Then
+                TextBox_Log.Text &= vbCrLf & ">>" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "【WevDav】上传成功  " & IO.Path.GetFileName(DesName)
+                Success = True
+            Else
+                TextBox_Log.Text &= vbCrLf & ">>" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "【WevDav】:上传失败:" & IO.Path.GetFileName(DesName)
+            End If
+        Catch ex As Exception
+            TextBox_Log.Text &= vbCrLf & ">>" & Format(Now, "yyyy-MM-dd HH:mm:ss") & "【WevDav】 上传错误." & ex.Message
+        End Try
+        Return Success
+    End Function
+End Class
